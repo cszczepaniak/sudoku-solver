09a2065064744320ecd1b74b71a688f49f71ae37
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-09-27 21:16:38 -0500

    take the lambda pipeline for a spin

--- pkg/rest/routes.go
+++ pkg/rest/routes.go
@@ -1,8 +1,15 @@
 package rest
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (s *Server) AddEndpoints(eng *gin.Engine) {
 	api := s.eng.Group(`/api`)
 	api.POST(`/solve`, s.solve)
+	api.GET(`/health`, func(c *gin.Context) {
+		c.String(http.StatusOK, `Healthy!`)
+	})
 }
