92a12456ab3300a136f6f15c8ed79e4dac9003a8
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-09-27 21:14:21 -0500

    get the command right

--- scripts/update-lambda.sh
+++ scripts/update-lambda.sh
@@ -3,4 +3,4 @@ set -ex
 
 source ./scripts/variables.sh
 
-aws update-function-code --function-name=$LAMBDA_NAME --s3-bucket=$ARTIFACT_BUCKET --s3-key=$BUILD_NAME.zip
+aws lambda update-function-code --function-name=$LAMBDA_NAME --s3-bucket=$ARTIFACT_BUCKET --s3-key=$BUILD_NAME.zip
